7fda22c5e082daec9a9c1e1558be34f93f3ecb3a
DESKTOP-PUB5I70\Hyungil Kim <samljkh@naver.com>
2021-11-04 10:24:58 +0900

    -before test

---
+++ Assets/TestAd.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAd : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	public void Reset()
+	{
+		
+	}
+    public void MainAdAcess()
+    {
+
+    }
+    public void ShowMainAd()
+    {
+
+    }
+    public void RewardAdAcess()
+    {
+
+    }
+    public void ShowRewardAd()
+    {
+
+    }
+}
